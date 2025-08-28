0c02723f2b932044d46110fbda182b0529d1a041
Uzair Khan <uzairhk16@gmail.com>
2025-08-28 17:11:43 +0530

    solutions page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -130,7 +130,7 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+        <hr className="my-6 border-gray-300 sm:mx-auto lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center">
             © 2023 &nbsp;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -22,18 +22,223 @@ export default function Home() {
       <div className="office w-full ">
         <img src={lucknow} alt="/" />
       </div>
-      <div className="solutions flex justify-between">
-        <div className="sol">
-          <span className="our">OUR</span>
-          <span className="solution"> SOLUTIONS</span>
+      <div className="solutions">
+        <div className=" flex justify-center">
+          <div className="sol">
+            <span className="fontt our sm:text-5xl md:text-6xl lg:text-8xl">
+              OUR
+            </span>
+            <span className="fontt solution sm:text-5xl md:text-6xl lg:text-8xl">
+              {" "}
+              SOLUTIONS.
+            </span>
+          </div>
         </div>
-        <div>
-          <p className="text-white text-lg w-2/5 mt-10">
-            Creating storied growth through customized solutions to match
-            individual Brand needs
-          </p>
+        <hr className="my-6 border-t-2 border-gray-300 sm:mx-auto lg:my-8 " />
+        <div className="flex justify-evenly ">
+          <div>
+            <h1 className="text-white font01 sm:text-2xl md:text-3xl mt-15">
+              01. &nbsp;IMMERSIVE TECHNOLOGY
+            </h1>
+          </div>
+          <div>
+            <ul className="text-gray-400">
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Experience Center
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Augmented Reality
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Virtual Reality
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Projection Mapping
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Holographic Technology
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Spatial Computing
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p>
+              <i className="fa-solid fa-arrow-up-right-from-square text-white sm:text-2xl md:text-5xl ml-30"></i>
+            </p>
+          </div>
         </div>
-        
+        <hr className="my-6 border-gray-300 sm:mx-auto lg:my-8 " />
+        <div className="flex justify-evenly mt-10 mb-10">
+          <div>
+            <h1 className="text-white font01 sm:text-2xl md:text-3xl mt-15">
+              02. &nbsp;EXPERIANTIAL MARKETING
+            </h1>
+          </div>
+          <div>
+            <ul className="text-gray-400">
+              <li>
+                <i class="fa-solid fa-caret-up"></i> CGI CAMPAIGN
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Motion capture technology
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Interactive tech-led
+                campaigns
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> IoT/Custom Solutions
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Photobooth
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Kiosk/Vending Machine
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p>
+              <i className="fa-solid fa-arrow-up-right-from-square text-white sm:text-2xl md:text-5xl ml-30"></i>
+            </p>
+          </div>
+        </div>
+        <hr className="my-6 border-t-2 border-gray-300 sm:mx-auto lg:my-8 " />
+        <div className="flex justify-evenly mt-10 mb-10">
+          <div>
+            <h1 className="text-white font01 sm:text-2xl md:text-3xl mt-15">
+              03. &nbsp;INTERACTIVE SOLUTION
+            </h1>
+          </div>
+          <div>
+            <ul className="text-gray-400">
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Interactive Wall /
+                Interactive Screen
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Interactive Table
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Interactive Book
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Object Recognition Tabl
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Holographic Technology
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> 3D Configurators
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p>
+              <i className="fa-solid fa-arrow-up-right-from-square text-white sm:text-2xl md:text-5xl ml-30"></i>
+            </p>
+          </div>
+        </div>
+        <hr className="my-6 border-t-2 border-gray-300 sm:mx-auto lg:my-8 " />
+        <div className="flex justify-evenly mt-10 mb-10">
+          <div>
+            <h1 className="text-white font01 sm:text-2xl md:text-3xl mt-11">
+              04. &nbsp;MOBILE APPS & GAMES
+            </h1>
+          </div>
+          <div>
+            <ul className="text-gray-400">
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Web Development
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> App Development
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Artificial Intelligence
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Game Development
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> UI/UX Design
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p>
+              <i className="fa-solid fa-arrow-up-right-from-square text-white sm:text-2xl md:text-5xl ml-30"></i>
+            </p>
+          </div>
+        </div>
+        <hr className="my-6 border-t-2 border-gray-300 sm:mx-auto lg:my-8 " />
+        <div className="flex justify-evenly mt-10 mb-10">
+          <div>
+            <h1 className="text-white font01 sm:text-2xl md:text-3xl mt-15">
+              05. &nbsp;CONTENT DEVELOPMENT
+            </h1>
+          </div>
+          <div>
+            <ul className="text-gray-400">
+              <li>
+                <i class="fa-solid fa-caret-up"></i> 3D/2D Animations
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Motion Graphics
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Architectural Visualization
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Explainer Videos
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Holographic Technology
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Anamorphic Content
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p>
+              <i className="fa-solid fa-arrow-up-right-from-square text-white sm:text-2xl md:text-5xl ml-30"></i>
+            </p>
+          </div>
+        </div>
+        <hr className="my-6 border-t-2 border-gray-300 sm:mx-auto lg:my-8 " />
+        <div className="flex justify-evenly mt-10 mb-10">
+          <div>
+            <h1 className="text-white font01 sm:text-2xl md:text-3xl mt-8 mb-10">
+              06. &nbsp; STRATEGY & DESIGN
+            </h1>
+          </div>
+          <div>
+            <ul className="text-gray-400">
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Scenario Mapping & Brand
+                Audits
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Graphic Design & Prototype
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Competitor Mapping
+              </li>
+              <li>
+                <i class="fa-solid fa-caret-up"></i> Brand Identity
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p>
+              <i className="fa-solid fa-arrow-up-right-from-square text-white sm:text-2xl md:text-5xl ml-30"></i>
+            </p>
+          </div>
+        </div>
+        <hr className="my-6 border-t-2 border-gray-300 sm:mx-auto lg:my-8 " />
       </div>
     </div>
   );
